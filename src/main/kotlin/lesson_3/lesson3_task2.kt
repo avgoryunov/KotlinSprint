8f6798e78427fedbc3e255f5433a5ef4d68b0a39
avgoryunov <av.goryunov58@gmail.com>
2024-12-16 21:08:46 +0300

    KS-3-2 personal data созданы переменные, в консоль выведены данные в формате (ФИО, возраст)

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main () {
+
+    var familyName = "Андреева"
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$familyName $name $middleName, $age лет")
+
+    familyName = "Сидорова"
+    age = 22
+
+    println("$familyName $name $middleName, $age лет")
+
+}
