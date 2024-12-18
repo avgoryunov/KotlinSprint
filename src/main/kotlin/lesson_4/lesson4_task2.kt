606c16b9d5913f711922b3929f1dd90a9c3a760b
avgoryunov <av.goryunov58@gmail.com>
2024-12-18 16:28:18 +0300

    KS-4-2 check cargo созданы переменные, с помощью операторов сравнения определено соответствие данных груза категории "Average". В консоль выведены строки в требуемом формате. Параметры "Average" перенесены в константы.

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -2,20 +2,21 @@ package org.example.lesson_4
 
 fun main () {
 
-    val averageWeightMin = 35
-    val averageWeightMax = 100
-    val averagevolume = 100
     var weightCargo = 20
     var volumeCargo = 80
 
-    var averageCategory = (weightCargo > averageWeightMin) && (weightCargo <= averageWeightMax) && (volumeCargo < averagevolume)
+    var averageCategory = (weightCargo > AVERAGE_WEIGHT_MIN) && (weightCargo <= AVERAGE_WEIGHT_MAX) && (volumeCargo < AVERAGE_VOLUME)
     println("Груз с весом $weightCargo кг и объемом $volumeCargo л соответствует категории 'Average': $averageCategory")
 
     weightCargo = 50
     volumeCargo = 100
 
-    averageCategory = (weightCargo > averageWeightMin) && (weightCargo <= averageWeightMax) && (volumeCargo < averagevolume)
+    averageCategory = (weightCargo > AVERAGE_WEIGHT_MIN) && (weightCargo <= AVERAGE_WEIGHT_MAX) && (volumeCargo < AVERAGE_VOLUME)
 
     println("Груз с весом $weightCargo кг и объемом $volumeCargo л соответствует категории 'Average': $averageCategory")
 
 }
+
+const val AVERAGE_WEIGHT_MIN = 35
+const val AVERAGE_WEIGHT_MAX = 100
+const val AVERAGE_VOLUME = 100
