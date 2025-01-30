46f3afd0bb4d716f3bb74c74eb07f755c78ba205
avgoryunov <av.goryunov58@gmail.com>
2025-01-30 19:37:43 +0300

    KS-6-2 timer rework Программа запрашивает количество секунд для обратного отсчета. Ввод данных выполняется через консоль. На данный период времени устанавливается пауза. После окончания паузы выводится сообщение.

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,30 +1,9 @@
 package org.example.lesson_6
 
 fun main() {
-
     print("Введите количество секунд: ")
-    var secNumIn = readln().toInt()
-    var secNumOut = ZERO_VALUE
-
-    while (secNumIn > 0) {
-        secNumIn--
-        secNumOut++
-        Thread.sleep(1000)
-    }
-
-    if (secNumOut <= 14) {
-        when (secNumOut) {
-            1 -> println("Прошлa $secNumOut секундa.")
-            2, 3, 4 -> println("Прошло $secNumOut секунды.")
-            else -> println("Прошло $secNumOut секунд.")
-        }
-    } else {
-        when (secNumOut.toString().last()) {
-            '1' -> println("Прошлa $secNumOut секундa.")
-            '2', '3', '4' -> println("Прошло $secNumOut секунды.")
-            else -> println("Прошло $secNumOut секунд.")
-        }
-    }
+    val secNumIn = readln().toInt()
+    repeat(secNumIn) {Thread.sleep(1000)}
+    println("Прошло $secNumIn секунд.")
 }
 
-const val ZERO_VALUE = 0
