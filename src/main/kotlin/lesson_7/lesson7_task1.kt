cb4d5982a5069a4b6739e247020740366c83065a
avgoryunov <av.goryunov58@gmail.com>
2025-02-02 15:51:40 +0300

    KS-7-1 password generator rework Программа генерирует пароль с чередующимися строчными буквами латинского алфавита и числами. Общее количество знаков - 6. Пароль записывается в строковую переменную и выводится в консоль.

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,9 +2,11 @@ package org.example.lesson_7
 
 fun main() {
 
-    for (i in 3 downTo 1) {
-        val letterRan: Char = ('a'..'z').random()
-        val numberRan: Int = (1..9).random()
-        print("$letterRan$numberRan")
-    }
+    val letterRan: CharRange = 'a'..'z'
+    val numberRan: IntRange = 0..9
+    var password = ""
+
+    for (i in 3 downTo 1) password += "${letterRan.random()}${numberRan.random()}"
+
+    println(password)
 }
