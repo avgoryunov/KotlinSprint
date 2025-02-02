79b0d82c05d52ae832f981cacd4e36606e27cc05
avgoryunov <av.goryunov58@gmail.com>
2025-02-02 16:45:10 +0300

    KS-7-5 password generator rework Программа запрашивает длину пароля. Пользователь через консоль вводит значение. Если число меньше 6 запрос повторяется до выполнения условия. После получения значения длины пароля программа генерирует в случайном порядке цифры, строчные и прописные буквы, а также их расположение. Пароль записывается в строковую переменную и выводится в консоль.

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -2,17 +2,20 @@ package org.example.lesson_7
 
 fun main () {
 
-    var passwordLength : Int
-    do {
-        print("Введите длину пароля (должно быть не менее 6 символов): ")
-        passwordLength = readln().toInt()
-    } while (passwordLength < 6)
+    var passwordLength = 0
 
-    for (i in passwordLength downTo 1) {
-        val smallLetterRan: Char = ('a'..'z').random()
-        val bigLetterRan: Char = ('A'..'Z').random()
-        val numberRan: Int = (1..9).random()
-        val list = listOf(smallLetterRan, bigLetterRan, numberRan).random()
-        print(list)
+    while (passwordLength < 6) {
+        print("Введите длину пароля (минимальная длина 6 символов): ")
+        passwordLength = readln().toInt()
     }
+
+    val smallLetterRan: CharRange = 'a'..'z'
+    val bigLetterRan: CharRange = 'A'..'Z'
+    val numberRan: IntRange = 0..9
+    var password = ""
+
+    for (i in passwordLength downTo 1)
+        password += arrayOf(smallLetterRan.random(), bigLetterRan.random(), numberRan.random()).random()
+
+    println(password)
 }
