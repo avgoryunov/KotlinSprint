0a5c63366574b33a30ae2151243fd53f75d61e7f
avgoryunov <av.goryunov58@gmail.com>
2025-02-08 17:00:08 +0300

    KS-7-5 password generator rework Программа запрашивает длину пароля. Пользователь через консоль вводит значение. Если число меньше 6 запрос повторяется до выполнения условия. После получения значения длины пароля программа генерирует в случайном порядке цифры, строчные и прописные буквы, а также их расположение и выводит пароль в консоль.

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -9,13 +9,22 @@ fun main () {
         passwordLength = readln().toInt()
     }
 
-    val smallLetterRan: CharRange = 'a'..'z'
-    val bigLetterRan: CharRange = 'A'..'Z'
-    val numberRan: IntRange = 0..9
+    val bigLetterRan = 'A'..'Z'
+    val smallLetterRan = 'a'..'z'
+    val numberRan = 0..9
+    val passwordList: MutableList<String> = mutableListOf()
     var password = ""
 
-    for (i in passwordLength downTo 1)
-        password += arrayOf(smallLetterRan.random(), bigLetterRan.random(), numberRan.random()).random()
+    passwordList.add(bigLetterRan.random().toString())
+    passwordList.add(smallLetterRan.random().toString())
+    passwordList.add(numberRan.random().toString())
 
+    for (i in 4 .. passwordLength) {
+        val listOfCharacters = listOf(bigLetterRan.random(), smallLetterRan.random(), numberRan.random()).random()
+        passwordList.add(listOfCharacters.toString())
+    }
+    passwordList.shuffle()
+
+    for (i in passwordList.indices) password += passwordList[i]
     println(password)
 }
