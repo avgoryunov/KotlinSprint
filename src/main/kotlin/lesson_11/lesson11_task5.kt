dfa432d63263ab178ec3dac93eb6d98ca24a7474
avgoryunov <av.goryunov58@gmail.com>
2025-03-08 20:59:37 +0300

    KS-11-5 forum rework Для приложения форум были созданы следующие сущности для хранения данных: - форум; - член форума; - сообщение форума. В процессе проектирования были созданы классы, свойства классов и методы. Также для проведения тестов были созданы экземпляры классов. Результаты работы приложения выведены в консоль.

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -2,26 +2,23 @@ package org.example.lesson_11
 
 data class Forum(
     val userList: MutableList<MemberOfTheForum> = mutableListOf(),
-    var user: MutableList<MemberOfTheForum> = mutableListOf(),
-    var userMessage: MutableList<ForumMessage> = mutableListOf(),
-
-    ) {
+    val userMessage: MutableList<ForumMessage> = mutableListOf(),
+) {
 
     fun createNewUser(memberOfTheForum: MemberOfTheForum) {
-        user.clear()
-        user.add(memberOfTheForum)
         memberOfTheForum.userId = userList.size + 1
         userList.add(memberOfTheForum)
     }
 
     fun createNewMessage(forumMessage: ForumMessage) {
-        val id: MutableList<String> = mutableListOf()
-        for (i in userList.indices) id.add(userList[i].userId.toString())
-        if (id.contains(forumMessage.authorId.toString())) {
-            userMessage.add(forumMessage)
-            printThread(forumMessage)
+        for (i in userList.indices) {
+            if (userList[i].userId == forumMessage.authorId) {
+                userMessage.add(forumMessage)
+                printThread(forumMessage)
+                return
+            }
         }
-        else println("Для отправки сообщений необходимо зарегистрироваться")
+        println("Для отправки сообщений необходимо зарегистрироваться")
     }
 
     private fun printThread(forumMessage: ForumMessage) {
@@ -30,72 +27,25 @@ data class Forum(
 }
 
 data class MemberOfTheForum(
-    var userId: Int,
+    var userId: Int = 0,
     val userName: String,
 )
-
-class MemberOfTheForumBuilder {
-    private var userId: Int = 0
-    private var userName: String = "User"
-
-    fun setUserName(userName: String) = apply { this.userName = userName }
-
-    fun build():MemberOfTheForum {
-        return MemberOfTheForum(userId, userName)
-    }
-}
 
 data class ForumMessage(
     var authorId: Int,
     var message: String,
 )
 
-class ForumMessageBuilder {
-    private var authorId: Int = 0
-    private var message: String = "Message"
-
-    fun setAuthorId(authorId: Int) = apply { this.authorId = authorId }
-    fun setMessage(message: String) = apply { this.message = message }
-
-    fun build():ForumMessage {
-        return ForumMessage(authorId, message)
-    }
-}
-
 fun main() {
 
-    val memberOfTheForum1 = MemberOfTheForumBuilder()
-        .setUserName("User1")
-        .build()
+    val memberOfTheForum1 = MemberOfTheForum(userName = "User1")
+    val memberOfTheForum2 = MemberOfTheForum(userName = "User2")
 
-    val memberOfTheForum2 = MemberOfTheForumBuilder()
-        .setUserName("User2")
-        .build()
-
-    val forumMessage1 = ForumMessageBuilder()
-        .setAuthorId(1)
-        .setMessage("Сообщение пользователя 1")
-        .build()
-
-    val forumMessage2 = ForumMessageBuilder()
-        .setAuthorId(2)
-        .setMessage("Сообщение пользователя 2")
-        .build()
-
-    val forumMessage3 = ForumMessageBuilder()
-        .setAuthorId(1)
-        .setMessage("Новое сообщение пользователя 1")
-        .build()
-
-    val forumMessage4 = ForumMessageBuilder()
-        .setAuthorId(2)
-        .setMessage("Новое сообщение пользователя 2")
-        .build()
-
-    val forumMessage5 = ForumMessageBuilder()
-        .setAuthorId(3)
-        .setMessage("Сообщение пользователя 3")
-        .build()
+    val forumMessage1 = ForumMessage(authorId = 1, message = "Сообщение пользователя 1")
+    val forumMessage2 = ForumMessage(authorId = 2, message = "Сообщение пользователя 2")
+    val forumMessage3 = ForumMessage(authorId = 1, message = "Новое сообщение пользователя 1")
+    val forumMessage4 = ForumMessage(authorId = 2, message = "Новое сообщение пользователя 2")
+    val forumMessage5 = ForumMessage(authorId = 3, message = "Сообщение пользователя 3")
 
     val forum1 = Forum()
 
