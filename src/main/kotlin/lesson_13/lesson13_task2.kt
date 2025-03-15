15c62f224bbae7520504a40547a27ef3e5c2735a
avgoryunov <av.goryunov58@gmail.com>
2025-03-15 19:22:52 +0300

    KS-13-2 phone directory Создан класс для хранения данных пользователя телефонного справочника. В конструкторе класса созданы переменные с указанием типа. Переменная "company" (через оператор безопасного вызова) по умолчанию является null. Созданы и проинициализированы экземпляры класса. Результаты работы приложения выведены в консоль.

---
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_13
+
+data class Subscriber1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+
+    fun outputToTheConsole() {
+        if (company != null) println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: $company\n")
+        else println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: <не указано>\n")
+    }
+}
+
+fun main() {
+
+    val subscriber11 = Subscriber1(
+        name = "user1",
+        phoneNumber = 11111111111,
+        company = "company1",
+    )
+
+    val subscriber12 = Subscriber1(
+        name = "user2",
+        phoneNumber = 22222222222,
+    )
+
+    subscriber11.outputToTheConsole()
+    subscriber12.outputToTheConsole()
+}
