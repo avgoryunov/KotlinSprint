c5da4e0f2817e6bb3280965a098d4055e8d0a7b8
avgoryunov <av.goryunov58@gmail.com>
2025-04-28 22:08:03 +0300

    KS-18-5 draw shapes rework2 Создан класс "Screen" с методами "drawCircle", "drawSquare" и "drawPoint". Для изменения типов координат фигур использован полиморфизм с типом Ad hoc. Создан экземпляр класса. Вызваны методы рисования геометрических фигур, которые возвращают строку с названием фигуры и указанием ее координат. Результат работы приложения выведен в консоль.

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -5,42 +5,42 @@ class Screen {
     fun drawCircle(
         x: Int,
         y: Int,
-    ) : String {
+    ): String {
         return "Фигура \"круг\" нарисована в координатах ($x, $y)"
     }
 
     fun drawCircle(
         x: Float,
         y: Float,
-    ) : String {
+    ): String {
         return "Фигура \"круг\" нарисована в координатах ($x, $y)"
     }
 
     fun drawSquare(
         x: Int,
         y: Int,
-    ) : String {
+    ): String {
         return "Фигура \"квадрат\" нарисована в координатах ($x, $y)"
     }
 
     fun drawSquare(
         x: Float,
         y: Float,
-    ) : String {
+    ): String {
         return "Фигура \"квадрат\" нарисована в координатах ($x, $y)"
     }
 
     fun drawPoint(
         x: Int,
         y: Int,
-    ) : String {
+    ): String {
         return "Фигура \"точка\" нарисована в координатах ($x, $y)"
     }
 
     fun drawPoint(
         x: Float,
         y: Float,
-    ) : String {
+    ): String {
         return "Фигура \"точка\" нарисована в координатах ($x, $y)"
     }
 }
@@ -48,14 +48,14 @@ class Screen {
 fun main() {
     val screen1 = Screen()
 
-    val circle1 = screen1.drawCircle( 1, 4)
-    val circle2 = screen1.drawCircle( 1.5f, 4.5f)
+    val circle1 = screen1.drawCircle(1, 4)
+    val circle2 = screen1.drawCircle(1.5f, 4.5f)
 
-    val square1 = screen1.drawSquare( 2, 6)
-    val square2 = screen1.drawSquare( 2.5f, 6.5f)
+    val square1 = screen1.drawSquare(2, 6)
+    val square2 = screen1.drawSquare(2.5f, 6.5f)
 
-    val point1 = screen1.drawPoint( 3, 9)
-    val point2 = screen1.drawPoint( 3.5f, 9.5f)
+    val point1 = screen1.drawPoint(3, 9)
+    val point2 = screen1.drawPoint(3.5f, 9.5f)
 
     val list1 = listOf(circle1, circle2, square1, square2, point1, point2)
 
