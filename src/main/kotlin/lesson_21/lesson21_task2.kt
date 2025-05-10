739e8629de8a962c6d16cbe810ddf07a0653d0fe
avgoryunov <av.goryunov58@gmail.com>
2025-05-10 16:20:16 +0300

    KS-21-2 the sum of even numbers rework Создана extension функция "evenNumbersSum" для класса "MutableList<Int>". Функция возвращает сумму всех четных чисел в списке. Создан экземпляр класса. Результат работы приложения выведен в консоль.

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -7,10 +7,5 @@ fun main() {
 }
 
 fun MutableList<Int>.evenNumbersSum(): Int {
-    var sum = 0
-
-    for (number in this) {
-        if (number % 2 == 0) sum += number
-    }
-    return sum
+    return this.filter { it % 2 == 0 }.sum()
 }
